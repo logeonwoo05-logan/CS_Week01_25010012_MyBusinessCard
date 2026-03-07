55248c31f754f72c7f3909b51f9bf0f220524b36
Logan <Logan@DESKTOP-FHVQ7AN>
2026-03-07 15:31:30 +0900

    초기 커밋.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace CS_week01_25010012_MyBusinessCard
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
